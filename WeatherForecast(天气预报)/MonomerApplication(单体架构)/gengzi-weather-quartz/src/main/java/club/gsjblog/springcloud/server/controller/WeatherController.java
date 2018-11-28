4a11ae9b6726ec12c67d7b41b13327e09674666e
gengzi <1164014750@qq.com>
2018-11-28 22:29:31 +0800

    天气预报项目 version1.2 增加quartz

---
+++ WeatherForecast(天气预报)/MonomerApplication(单体架构)/gengzi-weather-quartz/src/main/java/club/gsjblog/springcloud/server/controller/WeatherController.java
@@ -0,0 +1,48 @@
+package club.gsjblog.springcloud.server.controller;
+
+import club.gsjblog.springcloud.server.bean.WeatherResponse;
+import club.gsjblog.springcloud.server.service.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 天气预报controller
+ *
+ * @date 2018年11月27日22:52:53
+ */
+@RestController
+@RequestMapping("weather")
+public class WeatherController {
+
+    @Autowired
+    private WeatherService weatherService;
+
+    /**
+     * 根据城市名称，查询天气信息
+     *
+     * @param cityname 城市名称
+     * @return WeatherResponse
+     */
+    @RequestMapping(value = "/cityName/{cityname}", method = RequestMethod.GET)
+    public WeatherResponse getWeatherByCityName(@PathVariable("cityname") String cityname) {
+        return weatherService.getWeatherByCityName(cityname);
+    }
+
+
+    /**
+     * 根据城市id，查询天气信息
+     *
+     * @param cityId 城市id
+     * @return WeatherResponse
+     */
+    @RequestMapping(value = "/cityId/{cityId}", method = RequestMethod.GET)
+    public WeatherResponse getWeatherByCityId(@PathVariable("cityId") String cityId) {
+        return weatherService.getWeatherByCityId(cityId);
+    }
+
+
+}
---
+++ WeatherForecast(天气预报)/MonomerApplication(单体架构)/gengzi-weather-quartz/src/main/java/club/gsjblog/springcloud/server/service/impl/WeatherServiceImpl.java
@@ -0,0 +1,127 @@
+package club.gsjblog.springcloud.server.service.impl;
+
+import club.gsjblog.springcloud.server.bean.WeatherResponse;
+import club.gsjblog.springcloud.server.service.WeatherService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.HttpClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.function.BinaryOperator;
+
+/**
+ * 天气服务service 的实现类
+ */
+@Service
+public class WeatherServiceImpl implements WeatherService {
+    //导入日志
+    Logger logger =  LoggerFactory.getLogger(WeatherServiceImpl.class);
+    //超时时间
+    private static final long TIMEOUT = 1800L;
+
+    @Value(value = "${weather.uri}")
+    private String publicWeatherUri;
+
+    //Spring dtat redis 提供的对String 类型提供的
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private RestTemplate restTemplate;  // Spring 中对 Httpclient 的封装
+
+
+    /**
+     * 根据城市名称，查询天气信息
+     *
+     * @param cityname 城市名称
+     * @return WeatherResponse
+     */
+    @Override
+    public WeatherResponse getWeatherByCityName(String cityname) {
+        return getWeatherResponse("?city=" + cityname);
+    }
+
+
+    /**
+     * 根据城市id，查询天气信息
+     *
+     * @param cityId 城市id
+     * @return WeatherResponse
+     */
+    @Override
+    public WeatherResponse getWeatherByCityId(String cityId) {
+        return getWeatherResponse("?citykey=" + cityId);
+    }
+
+
+    private WeatherResponse getWeatherResponse(String citycode) {
+
+        //天气数据的特点，基本半小时更新一次数据。
+        //根据citycode 或者 cityid 查询redis中是否存在
+        //存在，从Redis中获取数据
+        //不存在，调用第三方服务,并写入缓存
+        String weatherUri = publicWeatherUri + citycode;
+        System.out.println(weatherUri);
+        String body = null;
+        ObjectMapper obj = new ObjectMapper();
+        WeatherResponse weatherResponse = null;
+        ValueOperations<String, String> stringStringValueOperations = stringRedisTemplate.opsForValue();
+        if (stringRedisTemplate.hasKey(citycode)) {
+            logger.info("读缓存");
+            body = stringStringValueOperations.get(citycode);
+        } else {
+            ResponseEntity<String> forEntity = restTemplate.getForEntity(weatherUri, String.class);
+            if (forEntity.getStatusCodeValue() == 200) {
+                logger.info("调用接口");
+                body = forEntity.getBody();
+                //10毫秒过期
+                stringStringValueOperations.set(citycode, body,TIMEOUT, TimeUnit.SECONDS);
+            }
+        }
+        try {
+            weatherResponse = obj.readValue(body, WeatherResponse.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return weatherResponse;
+    }
+
+
+    @Override
+    public void syncDateByCityId(String cityId) {
+        String weatherUri = publicWeatherUri + "?citykey=" + cityId;
+        this.saveWeatherData(weatherUri);
+    }
+
+    /**
+     * 把天气数据放在缓存
+     * @param uri
+     */
+    private void saveWeatherData(String uri) {
+        String key = uri;
+        String strBody = null;
+        ValueOperations<String, String>  ops = stringRedisTemplate.opsForValue();
+
+        // 调用服务接口来获取
+        ResponseEntity<String> respString = restTemplate.getForEntity(uri, String.class);
+
+        if (respString.getStatusCodeValue() == 200) {
+            strBody = respString.getBody();
+        }
+
+        // 数据写入缓存
+        ops.set(key, strBody, TIMEOUT, TimeUnit.SECONDS);
+
+    }
+
+
+}
